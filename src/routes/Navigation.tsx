3c516f8e53286f15b8ea048ed4666567c3eaa991
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-12-04 21:14:04 -0500

    Componentes Lazy agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, NavLink, Navigate, Route, Routes } from 'react-router-dom';
 import logo from '../logo.svg';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 export const Navigation = () => {
   return (
@@ -9,21 +10,21 @@ export const Navigation = () => {
           <img src={logo} alt="React logo" />
           <ul>
             <li>
-              <NavLink to='/home'>Home</NavLink>
+              <NavLink to='/lazy1'>Lazy1</NavLink>
             </li>
             <li>
-              <NavLink to='/about'>About</NavLink>
+              <NavLink to='/lazy2'>Lazy2</NavLink>
             </li>
             <li>
-              <NavLink to='/users'>Users</NavLink>
+              <NavLink to='/lazy3'>Lazy3</NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path='about' element={<h1>About page</h1>} />
-          <Route path='users' element={<h1>Users page</h1>} />
-          <Route path='/home' element={<h1>Home page</h1>} />
+          <Route path='lazy1' element={<LazyPage1 />} />
+          <Route path='lazy2' element={<LazyPage2 />} />
+          <Route path='lazy3' element={<LazyPage3 />} />
           <Route path='/*' element={<Navigate to='/home' replace />} />
         </Routes>
       </div>
